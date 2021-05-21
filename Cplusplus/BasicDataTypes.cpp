8cc86518e1df196065d842bdad6b76864bab050e
TheProgrammer565 <76843640+TheProgrammer565@users.noreply.github.com>
2021-05-21 22:29:29 +0530

    Cplusplus_Programs

---
+++ Cplusplus/BasicDataTypes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<"\n"<<b<<"\n"<<c<<endl;
+    printf("%.3f",d);
+    printf("\n%.9lf",e);    
+    return 0;
+}
